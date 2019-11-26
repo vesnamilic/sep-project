4c38d0ee5194a37fba96dd5f1fccbc304775c1c7
vesnamilic <44587403+vesnamilic@users.noreply.github.com>
2019-11-26 21:36:49 +0100

    Dodat mock servis bitcoin

---
+++ mock-bitcoin-service/src/main/java/sep/project/MockBitcoinServiceApplication.java
@@ -0,0 +1,15 @@
+package sep.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MockBitcoinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockBitcoinServiceApplication.class, args);
+	}
+
+}
