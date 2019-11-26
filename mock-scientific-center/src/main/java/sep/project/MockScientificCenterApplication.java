930bccdd4a9bfd9a099fe82e233c275b3c91ac8e
vesnamilic <44587403+vesnamilic@users.noreply.github.com>
2019-11-26 18:39:31 +0100

    Dodati: eureka, zuul, config server, i mock bitcoin  mikroservisi

---
+++ mock-scientific-center/src/main/java/sep/project/MockScientificCenterApplication.java
@@ -0,0 +1,15 @@
+package sep.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MockScientificCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockScientificCenterApplication.class, args);
+	}
+
+}
---
+++ mock-scientific-center/src/test/java/sep/project/MockScientificCenterApplicationTests.java
@@ -0,0 +1,14 @@
+package sep.project;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockScientificCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
