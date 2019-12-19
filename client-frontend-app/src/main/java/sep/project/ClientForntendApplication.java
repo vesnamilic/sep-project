dc5c55ed4cc8c5a8d81cf7a6dcf8f0c7357f2a4e
matkovskim <marijanamatkovski@uns.ac.rs>
2019-12-19 09:45:06 +0100

    novi sertifikati i https na frontu

---
+++ client-frontend-app/src/main/java/sep/project/ClientForntendApplication.java
@@ -0,0 +1,13 @@
+package sep.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientForntendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientForntendApplication.class, args);
+	}
+
+}
